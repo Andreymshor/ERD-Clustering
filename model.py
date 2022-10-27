21b351a30392ae3d155541416e90f1190f88d17d
Andreymshor <andreyshor1999@gmail.com>
2022-10-27 15:48:57 -0400

    created first annotation

--- model.py
+++ model.py
@@ -1,2 +1,2 @@
 # make sure your labels in image are the same as the labels in here (along w capitalization)
-labels = ['entity', 'weak_entity', 'rel', 'ident_rel', 'rel_atri', 'many', 'one']
+labels = ['entity', 'weak_entity', 'rel', 'ident_rel', 'rel_attr', 'many', 'one']
