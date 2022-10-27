efd91d3d8cd445901738f94cfb6e913108b6048d
Andreymshor <andreyshor1999@gmail.com>
2022-10-27 16:41:43 -0400

    Update model.py

--- model.py
+++ model.py
@@ -7,5 +7,5 @@
 # rel_attr: an attribute that belongs to a relationship, looks like an oval/eclipse
 # many: appears at the end of a connection, has 3 lines coming out of it
 # one: appears at the end of a connection, has 1 or 2 vertical lines like so: ||
-# rel_type: used for connections that don't follow standard format. THese connections look like so: "1..*", "*..1", etc
+# rel_type: used for connections that don't follow standard format. These connections look like so: "1..*", "*..1", etc
 labels = ['entity', 'weak_entity', 'rel', 'ident_rel', 'rel_attr', 'many', 'one' , 'rel_type']
