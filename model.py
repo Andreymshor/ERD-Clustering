2122db69dc4c1399372ad688d62138d86961152b
Andreymshor <andreyshor1999@gmail.com>
2022-10-27 16:41:15 -0400

    updated documentation in model.py for labeling

--- model.py
+++ model.py
@@ -1,2 +1,11 @@
 # make sure your labels in image are the same as the labels in here (along w capitalization)
-labels = ['entity', 'weak_entity', 'rel', 'ident_rel', 'rel_attr', 'many', 'one']
+
+# entity: looks like a rectangle
+# weak entity: looks like 2 rectangles on top of each other
+# rel: a relationship: looks like a diamond
+# ident_rel: an identifying relationship: Looks like 2 diamonds on top of each other
+# rel_attr: an attribute that belongs to a relationship, looks like an oval/eclipse
+# many: appears at the end of a connection, has 3 lines coming out of it
+# one: appears at the end of a connection, has 1 or 2 vertical lines like so: ||
+# rel_type: used for connections that don't follow standard format. THese connections look like so: "1..*", "*..1", etc
+labels = ['entity', 'weak_entity', 'rel', 'ident_rel', 'rel_attr', 'many', 'one' , 'rel_type']
